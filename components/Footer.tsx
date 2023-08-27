aaf8ba1fb5d1a887f1d00ca0a9fd9e457116031e
Krishna Shah <85193239+DragonflyRobotics@users.noreply.github.com>
2023-08-27 15:41:28 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,17 +10,17 @@ export default function Footer() {
             <span className="white">Copyright © {year} <Link href="/">Krishna</Link> - All rights reserved. (Template by Blaiti)</span>
             <ul className="flex">
                 <li>
-                    <a href="https://www.linkedin.com/in/skanderblaiti" target="_blank" rel="noreferrer">
+                    <a href="https://www.linkedin.com/in/krishna-shah-193b0a255/" target="_blank" rel="noreferrer">
                         <Image src="/icons/linkedin.svg" width={24} height={24} alt="linkedin-icon" />
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/blaiti" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/DragonflyRobotics" target="_blank" rel="noreferrer">
                         <Image src="/icons/github.svg" width={24} height={24} alt="github-icon" />
                     </a>
                 </li>
                 <li>
-                    <a href="https://youtube.com/Blaiti" target="_blank" rel="noreferrer">
+                    <a href="https://www.youtube.com/channel/UC6nu2aGbAEX3yy5b0W9GBIg" target="_blank" rel="noreferrer">
                         <Image src="/icons/youtube.svg" width={24} height={24} alt="youtube-icon" />
                     </a>
                 </li>
